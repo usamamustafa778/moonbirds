a3575f323bcbaca57b90bb29b6aa909fab5b0e22
Usama Bhatti <70816277+usamamustafa778@users.noreply.github.com>
2022-05-19 22:30:40 -0700

    push

--- src/Components/Copyright.js
+++ src/Components/Copyright.js
@@ -11,15 +11,18 @@ function Copyright() {
             <span></span>
           </div>
           <div className="row">
-              <div className="col-md-3">
-                  {/* <img src="/images/proof.svg" alt="" /> */}
-                  {/* <p className="text-gray">© 2022 PROOF Holdings</p> */}
+            <div className="col-md-3">
+              {/* <img src="/images/proof.svg" alt="" /> */}
+              {/* <p className="text-gray">© 2022 PROOF Holdings</p> */}
+            </div>
+            <div className="col-md-6"></div>
+            <div className="col-md-3">
+              <p className="text-gray" style={{ fontSize: 14 }}>Follow along for updates:</p>
+              <div className="d-flex social-icons">
+                <a href="https://twitter.com/" className="px-1"><img src="/images/twitter.svg" alt="" style={{ height: 20 }} /></a>
+                <a href="https://discord.com/" className="px-1"><img src="/images/discord.svg" alt="" style={{ height: 20 }} /></a>
               </div>
-              <div className="col-md-6"></div>
-              <div className="col-md-3">
-                  <p className="text-gray">Follow along for updates:</p>
-                  <img src="/images/twitter.svg" alt="" style={{height:20}} />
-              </div>
+            </div>
           </div>
         </div>
       </div>
